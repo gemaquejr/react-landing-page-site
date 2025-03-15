3e249405022c46b44b1130afdf891727e68e59d9
Francisco Gemaque <gemaquejr@hotmail.com>
2025-03-15 14:21:41 -0300

    Adiciona componente Find
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import SearchBar from "./components/searchBar/Search";
+import Find from "./components/find/Find";
 
 function App() {
   return (
     <div>
       < Navbar />
       < SearchBar />
+      < Find />
     </div>
   );
 }
